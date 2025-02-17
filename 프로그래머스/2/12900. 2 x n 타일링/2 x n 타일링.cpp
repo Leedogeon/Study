6e633857d061c847bcdf9b63758a28bc024635d3
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-02-17 19:50:02 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.16 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    if(n < 3)
+    {
+        return n;
+    }
+    
+    int prev = 1;
+    int temp = 2;
+    
+    for(int i = 3; i <= n; i++)
+    {
+        answer = (prev + temp) % 1000000007;
+        prev = temp;
+        temp = answer;
+    }
+    return answer;
+}
