d715cdbd2df592a49a4b2ccebb26388149ba4457
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-06-12 17:24:41 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.19 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+           int[] answer = new int[2];
+
+            foreach(int num in num_list)
+            {
+                if (num % 2 == 0) answer[0] += 1;
+                else answer[1] += 1;
+            }
+        return answer;
+    }
+}
