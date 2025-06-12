0da5bbbba486e3a0abd2ba87886c6f24704c8589
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-06-12 16:48:20 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.50 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, string letter) {
+        string answer = "";
+        foreach(char c in my_string)
+        {
+            if(c != letter[0])
+                answer+=c;
+        }
+        return answer;
+    }
+}
