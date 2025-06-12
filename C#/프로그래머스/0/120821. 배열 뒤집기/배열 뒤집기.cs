53f907a3079fd656110474ee9874d2a1ec93744f
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-06-12 17:16:55 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.55 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[] {};
+        Array.Reverse(num_list);
+        answer = num_list;
+        return answer;
+    }
+}
