a5c84daade930ebbd089d7ebad6a881e140afb51
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-06-13 15:04:56 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.39 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n) {
+        string answer = "";
+        foreach(char c in my_string)
+        {
+            for(int i = 0; i<n;i++)
+            {
+                answer+=c;
+            }
+        }
+        return answer;
+    }
+}
