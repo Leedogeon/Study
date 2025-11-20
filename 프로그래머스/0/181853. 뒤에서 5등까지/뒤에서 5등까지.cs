0f23ef0785941de662589f8dfd5fcb5ccb30610c
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-11-20 22:26:35 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 4.93 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Linq;
+public class Solution {
+    public int[] solution(int[] num_list) {
+    int[] answer = num_list.OrderBy(x => x).Take(5).ToArray();
+        return answer;
+    }
+}
