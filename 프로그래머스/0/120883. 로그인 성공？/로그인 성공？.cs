5586fdba136abbcb7fcf1c1ee99f8d41bf4f7ecd
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-11-26 17:42:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.24 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution {
+    public string solution(string[] id_pw, string[,] db) {
+        string answer = "";
+        
+        for(int i = 0;i<db.GetLength(0);i++)
+        {
+            if (db[i, 0] == id_pw[0])
+            {
+                if (db[i, 1] == id_pw[1])
+                {
+                    answer = "login";
+                    break;
+                }
+                else 
+                {
+                    answer = "wrong pw";
+                    break;
+                }
+            }
+            else answer = "fail";
+        }
+        return answer;
+    }
+}
