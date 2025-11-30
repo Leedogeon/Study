b36529e5c6ebda90e98d345c1be9ec43196cac56
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-11-30 15:44:08 +0900

    [level 0] Title: 최빈값 구하기, Time: 6.52 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+public class Solution {
+    public int solution(int[] array) {
+        var list = array.GroupBy(x => x, g => g, (x, g) => new { n = x, cnt = g.Count() });
+        var max = list.Where(x => x.cnt == list.Max(o => o.cnt));
+        int answer = max.Count() == 1 ? max.First().n : -1;
+        return answer;
+    }
+}
