1b0009dbe6c3a9f40504d6eea6ab17640e156143
Leedogeon <80244811+Leedogeon@users.noreply.github.com>
2025-11-30 15:10:18 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.32 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        for(int i = 1; i<arr.Length;i++)
+        {
+            answer *= arr[i]/gcd(answer,arr[i]);
+        }
+        
+        
+        return answer;
+    }
+    
+    int gcd(int a, int b)
+    {
+        if(b == 0) return a;
+        return(gcd(b,a%b));
+    }
+}
